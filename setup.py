b6dccd89647d136aa46496494ad45365badf5e92
Andrew Webster <andrew.webster@ValidateHealth.com>
2018-06-20 18:51:06 -0500

    master

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='actuary',
+    version='0.1',
+    packages=['Chapter_2_Core_Python.Chapter_2_Case_Study.rate_table'],
+    url='https://github.com/validatehealth/python-for-actuaries',
+    license='MIT',
+    author='Andrew Webster',
+    author_email='andrew.webster@validatehealth.com',
+    description='This is an actuarial toolkit as introduced by three Actex webinars on Python for Actuaries'
+)
